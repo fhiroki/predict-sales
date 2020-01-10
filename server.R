5d0268f209949dcd0e0b2b33645e27bfc7eb299b
Hiroki Fujioka <hiroki976@gmail.com>
2020-01-10 17:07:45 +0900

    create table_month and table_weather

--- server.R
+++ server.R
@@ -3,20 +3,44 @@
 library(shiny)
 
 
-df_shop <- read.csv("data/shop_2.csv", header=T)
+df_shop <- read.csv("data/shop.csv", header=T)
+df_shop_month <- read.csv("data/shop_year_month.csv", header=T)
 
 function(input, output, session) {
 
   updateSelectizeInput(           # selectizeInputウィジェット を 更新する．
     session,                      # update*()の時はこう書く決まり．
-    inputId = "shopNameInput",    # widget(input)のID
-    choices = df_shop$店名,       # 選択肢
+    inputId = "shopIdInput",    # widget(input)のID
+    choices = df_shop$shop_id,       # 選択肢
     server = T                    # サーバ側にこの情報を保存するかどうか
   )
 
   output$table <- renderTable(
-    df_shop[df_shop$店名 == input$shopNameInput, ]
+    df_shop[df_shop$shop_id == input$shopIdInput, ]
   )
+
+  output$table_month <- renderTable(
+      df_shop_month[df_shop_month$shop_id == input$shopIdInput &
+                        df_shop_month$year == 2019 &
+                        df_shop_month$month >= 3, ][c('month', 'hanbai', 'henpin')]
+  )
+
+  output$table_sum <- renderTable({
+      df_month = df_shop_month[df_shop_month$shop_id == input$shopIdInput &
+                        df_shop_month$year == 2019 &
+                        df_shop_month$month >= 3, ][c('month', 'hanbai', 'henpin')]
+      data.frame(
+          出荷数合計 = sum(df_month['hanbai']),
+          予測値との差分 = abs(100 - sum(df_month['hanbai'])))
+  })
+
+  output$table_weather <- renderTable({
+      columns = c("feel_avg_04", "sunny_cnt_04", "sunny_cnt_07", "rainy_cnt_07", "hummd_daily_min_03", "popu_per_shops")
+      data.frame(
+          "2018年" = df_shop[df_shop$year == 2018][columns],
+          "2019年" = df_shop[df_shop$yar == 2019][columns]
+      )
+  })
 
   output$text1 = renderText({ 
     as.character(1000)
--- ui.R
+++ ui.R
@@ -10,33 +10,22 @@ fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectizeInput(
-        inputId = "shopNameInput", label = h4("店名を選択"),
+        inputId = "shopIdInput", label = h4("店名を選択"),
         choices = NULL, selected = NULL 
         # 内部で読み込んだデータフレームの値をchoicesにしたいので空にしておく．
       ),
 
-      fileInput(
-        "file", label=h4("気象データをアップロード")
-      ),
-  
-      # ラッソ回帰で固定であれば必要なし.
-      radioButtons("model", label = h4("モデルを選択"),
-          choices = c("ラッソ回帰" = "lasso",
-                      "ランダムフォレスト" = "rf",
-                      "XGBoost" = "xgb"),
-          selected = "lasso"),
-
       br(),
-      h3("今年度の店出数量の予測値：1365")
+      h4("今年度の店出数量の予測値：1365")
     ),
 
     mainPanel(
         tabsetPanel(type = "tabs",
-                    tabPanel("店舗情報", br(), tableOutput("table")),
-                    tabPanel("グラフ"),
-                    tabPanel("分析"),
-                    tabPanel("要因"),
-                    tabPanel("モデル詳細"))
+                    tabPanel("店舗情報", br(), tableOutput("table"),
+                             tableOutput("table_month"), tableOutput("table_sum")),
+                    tabPanel("前年との気象の差", br(), tableOutput("table_weather")),
+                    tabPanel("モデル詳細"),
+                    tabPanel("全店舗の売上"))
     )
   )
 )
